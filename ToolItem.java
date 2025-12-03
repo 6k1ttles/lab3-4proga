b9269067bb5fd9987f826a65477b9dd2f08a2af0
6k1ttles <93944412+6k1ttles@users.noreply.github.com>
2025-12-03 23:23:23 +0300

    Update ToolItem.java

--- ToolItem.java
+++ ToolItem.java
@@ -1,6 +1,15 @@
+/**
+ * Класс для предметов-инструментов
+ * Реализует интерфейс {@link IPryTool}
+ */
 public class ToolItem extends Thing implements Tool {
     private final double leverageForce;
-
+    /**
+     * Создает инструмент
+     * @param name Название
+     * @param strength Прочность
+     * @param leverageForce Сила рычага (доп к весу персонажа)
+     */
     public ToolItem(String name, double strength, double leverageForce) {
         super(name, strength);
         this.leverageForce = leverageForce;
@@ -20,4 +29,5 @@ public class ToolItem extends Thing implements Tool {
     public String getStatsString() {
         return super.getStatsString() + ", Сила отткрытия: " + leverageForce;
     }
+
 }
