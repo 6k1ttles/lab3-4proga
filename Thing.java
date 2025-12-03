21d4a5321e36b408378b43ea69fdc857c4c2eaab
6k1ttles <93944412+6k1ttles@users.noreply.github.com>
2025-12-03 23:31:02 +0300

    Update Thing.java

--- Thing.java
+++ Thing.java
@@ -1,11 +1,19 @@
 import java.util.UUID;
 
+/**
+ * Абстрактный базовый класс для всех физических объектов
+ * Содержит свойства имя, прочность и механику разрушения
+ */
 public abstract class Thing {
     protected final String name;
     protected double strength;
     protected boolean isBroken = false;
     private final UUID id = UUID.randomUUID();
-
+    /**
+     * Создает новый предмет
+     * @param name Название предмета
+     * @param strength Начальная прочность
+     */
     public Thing(String name, double strength) {
         this.name = name;
         this.strength = strength;
@@ -18,25 +26,40 @@ public abstract class Thing {
     public double getStrength() {
         return strength;
     }
-
+    /**
+     * @return true, если предмет сломан и не может быть использован
+     */
     public boolean isBroken() {
         return isBroken;
     }
-
+    /**
+     * Абстрактный метод для получения типа предмета
+     * @return Строковое представление типа
+     */
     public abstract String getTypeName();
-
+    /**
+     * Формирует строку с хар-ками предмета
+     * @return Строка вида "[Тип] Имя (Прочность: X)"
+     */
     public String getStatsString() {
         return String.format("%s %s (Прочность: %.1f)",
                 getTypeName(), name, strength);
     }
-
+    /**
+     * Применяет нагрузку к предмету -> уменьшая его прочность
+     * Если нагрузка превышает прочность -> предмет ломается
+     *
+     * @param load Величина нагрузки
+     * @throws ItemBrokenException Если предмет ломается
+     */
     public void endureLoad(double load) throws BrokenItemException {
         if (isBroken) return;
-
+        // Уменьшение прочности предмета
         this.strength -= (load * 0.1);
         this.strength = Math.round(this.strength * 10.0) / 10.0;
-
-        if (load > (this.strength + 2.0) || this.strength <= 0) {
+         // Проверка на поломку
+        // или если прочность упала до нуля.
+        if (load > (this.strength + 0.1) || this.strength <= 0) {
             this.isBroken = true;
             this.strength = 0;
             throw new BrokenItemException("Предмет '" + name + "' сломался от нагрузки!");
@@ -60,4 +83,5 @@ public abstract class Thing {
     public String toString() {
         return name;
     }
+
 }
