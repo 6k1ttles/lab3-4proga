4aa9f25ccf3d0696988fda9d99a23e46ccf483e2
6k1ttles <93944412+6k1ttles@users.noreply.github.com>
2025-12-03 23:31:50 +0300

    Update TryResult.java

--- TryResult.java
+++ TryResult.java
@@ -1,2 +1,9 @@
+/**
+ * Запись для хранения результата попытки действия
+ *
+ * @param success Успешно ли завершилось действие
+ * @param message Текстовое описание результата
+ */
 public record TryResult(boolean success, String message) {
+
 }
