645b6c64631422f6de11f8845f127531fc298c9c
6k1ttles <93944412+6k1ttles@users.noreply.github.com>
2025-12-03 23:26:29 +0300

    Update Bottle.java

--- Bottle.java
+++ Bottle.java
@@ -1,7 +1,16 @@
+/**
+ * Класс целевого предмета — Кувшина
+ * Имеет состояние открыто/закрыто и порог силы для открытия
+ */
 public class Bottle extends Thing {
     private boolean isOpeen = false;
     private final double openingValue;
-
+     /**
+     * Создает бутылку
+     * @param name Название
+     * @param strength Прочность
+     * @param openingThreshold Сила в кг, необходимая для открытия
+     */
     public Bottle(String name, double strength, double openingValue) {
         super(name, strength);
         this.openingValue = openingValue;
@@ -19,7 +28,13 @@ public class Bottle extends Thing {
     public double getValueToCrash() {
         return openingValue;
     }
-
+    /**
+     * Попытка применить силу к пробке
+     * Если силы достаточно -> кувшин открывается
+     * Если нет -> сила уменьшает прочность кувшина
+     *
+     * @param force Приложенная сила в кг
+     */
     public void applyForce(double force) {
         if (isBroken || isOpeen) return;
 
@@ -29,6 +44,7 @@ public class Bottle extends Thing {
         } else {
             System.out.println("Пробка не поддается");
             try {
+                // Если не открылась -> 50% силы уходит в урон
                 this.endureLoad(force * 0.5);
             } catch (BrokenItemException e) {
                 System.out.println(e.getMessage());
@@ -36,4 +52,5 @@ public class Bottle extends Thing {
             }
         }
     }
+
 }
