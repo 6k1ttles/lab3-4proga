f375c5dc4f6dcf18dd0f7bf4204c17365f9c1aa7
6k1ttles <93944412+6k1ttles@users.noreply.github.com>
2025-12-03 23:34:22 +0300

    Update ThingType.java

--- ThingType.java
+++ ThingType.java
@@ -1,3 +1,7 @@
+/**
+ * Перечисление типов предметов
+ * Для создания экземпляров этих предметов
+ */
 public enum ThingType {
     KNIFE("Ножичек", 15.0, 5.0, true),
     BONE("Кость", 150.0, 40.0, true),
@@ -8,18 +12,29 @@ public enum ThingType {
     private final double strength;
     private final double actionValue;
     private final boolean isTool;
-
+    /**
+     * Конструктор элемента перечисления
+     * @param name Название предмета
+     * @param strength Начальная прочность предмета
+     * @param actionValue Сила рычага или урон удара
+     * @param isTool Является ли предмет рычагом
+     */
     ThingType(String name, double strength, double actionValue, boolean isTool) {
         this.name = name;
         this.strength = strength;
         this.actionValue = actionValue;
         this.isTool = isTool;
     }
-
+    /**
+     * @return Название типа предмета
+     */
     public String get_name() {
         return name;
     }
-
+     /**
+     * Метод для создания объекта предмета
+     * @return Экземпляр {@link PryToolItem} или {@link HammerItem} (зависит от флага isTool)
+     */
     public Thing create() {
         if (isTool) {
             return new ToolItem(name, strength, actionValue);
@@ -27,4 +42,5 @@ public enum ThingType {
             return new HammerItem(name, strength, actionValue);
         }
     }
+
 }
